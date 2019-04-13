41b750b7b868778800250066b73fefbac8ce7291
ninthwalker <ninthwalker@gmail.com>
2019-04-13 00:47:09 -0700

    Update index.php

--- root/config/www/index.php
+++ root/config/www/index.php
@@ -1,17 +1,20 @@
 <?php
-# Get Youtube URL from bookmark
+# Get Youtube URL from bookmarklet
+# Bookmarklet example code: javascript:location.href='http://yourIP:yourPort/index.php?v='+document.location.href;
 $cmd = 'youtube-dl --config-location /config/youtube-dl.conf '.$_REQUEST['v'];
 $url = $_REQUEST['v'];
 
 # date used for log
 $date = date("h:i:s");
-$log = "/downloads/logs/$url-$date.log";
+$log = "/downloads/logs/{$date}.log";
 
 # Run youtube-dl command in background and output to log
-shell_exec("$cmd &> $log &");
+shell_exec("$cmd > $log &");
 
 # TODO: redirect to webpage to show d/l status or maybe just back to originating URL
 #header('Location: status.php');
+
+# Read log
 echo "File Download has started for {$url} <p>";
 $file = escapeshellarg("$log");
 $line = `tail -n 1 $file`;
